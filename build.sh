e470dfac60fa9444011c7eb94c196918a0df646a
Fell-x27 <fell-x27@mail.ru>
2025-07-05 15:46:58 +0500

    Add Mainnet config and safeguard contract's reference UTxO in Exchange phase

--- build.sh
+++ build.sh
@@ -13,7 +13,7 @@ echo ""
 MAINNET_PATH="./artifacts/mainnet/"
 echo "MAINNET BUILD [${MAINNET_PATH}]"
 mkdir -p "$MAINNET_PATH"
-aiken build -t silent
+aiken build -t silent --env mainnet
 aiken blueprint convert > "$MAINNET_PATH/contract.script"
 aiken blueprint address --mainnet > "$MAINNET_PATH/contract.addr"
 aiken blueprint policy  > "$MAINNET_PATH/contract.pid"
